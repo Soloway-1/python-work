6017ab58cd13bbf49e97653f216cb5424f185b9d
Soloway-1 <poppmikola@gmail.com>
2024-07-13 11:00:14 +0300

    first commit

---
+++ github test.py
@@ -0,0 +1,7 @@
+my_name = input("Мене звати: ")
+city = input("Живу в селі: ")
+age = input("Мені: ")
+height = input("Десь приблизно: ")
+my_pet = input("Мій улюблинець це моя: ")
+Finale = f"Дуже коротко про себе. Мене звати({my_name})Мені({age})Живу в({city})Ріст({height})Люблю({my_pet}):)"
+print(Finale)
