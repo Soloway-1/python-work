e86602eb348165b94a1e497427d16af368de04ef
Soloway-1 <poppmikola@gmail.com>
2024-07-13 11:10:56 +0300

    test

--- github test.py
+++ github test.py
@@ -1,7 +1,1 @@
-my_name = input("Мене звати: ")
-city = input("Живу в селі: ")
-age = input("Мені: ")
-height = input("Десь приблизно: ")
-my_pet = input("Мій улюблинець це моя: ")
-Finale = f"Дуже коротко про себе. Мене звати({my_name})Мені({age})Живу в({city})Ріст({height})Люблю({my_pet}):)"
-print(Finale)
+input(add)
